2f37fd4308f396c69fd02bec3eedd9eb361c68a5
Ivan Christian <ivanchristian.kkuljaem@gmail.com>
2024-08-19 17:58:02 +0700

    Change About Description

--- src/app/about/AboutSection.js
+++ src/app/about/AboutSection.js
@@ -10,39 +10,46 @@ export default function AboutSection() {
             <div className="col-lg-6 col-md-12 p-0">
               <div className="about-content">
                 <span>About Us</span>
-                <h2>Short Story About Fovia Clinic Since 1999</h2>
+                <h2>Short Story About Kkuljaem Edu Since 2021</h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Quis ipsum suspendisse ultrices gravida. Risus commodo viverra
-                  maecenas accumsan lacus vel facilisis.
+                  Kkuljaem Edu adalah lembaga konsultasi studi ke Korea Selatan
+                  yang didirikan oleh Han Jongdae (Bandung Oppa) di tahun 2022.
+                  Hingga saat ini, kami telah berhasil membantu banyak siswa
+                  untuk berangkat dan belajar di Korea Selatan, dan terus
+                  mewujudkan komitmen kami untuk mewujudkan mimpi siswa untuk
+                  studi ke Korea Selatan.
                 </p>
-                <ul>
-                  <li>
-                    <i className="icofont-tick-mark"></i> Scientific Skills For
-                    getting a better result
-                  </li>
-                  <li>
-                    <i className="icofont-tick-mark"></i> Communication Skills
-                    to getting in touch
-                  </li>
-                  <li>
-                    <i className="icofont-tick-mark"></i> A Career Overview
-                    opportunity Available
-                  </li>
-                  <li>
-                    <i className="icofont-tick-mark"></i> A good Work
-                    Environment For work
-                  </li>
-                  <li>
-                    <i className="icofont-tick-mark"></i> Scientific Skills For
-                    getting a better result
-                  </li>
-                  <li>
-                    <i className="icofont-tick-mark"></i> Communication Skills
-                    to getting in touch
-                  </li>
-                </ul>
+                <div className="row">
+                  <div className="col-lg-6">
+                    <ul className="list">
+                      <li>
+                        <i className="fa fa-caret-right"></i>One-Stop Solution
+                      </li>
+                      <li>
+                        <i className="fa fa-caret-right"></i>Gratis Konsultasi
+                      </li>
+                      <li>
+                        <i className="fa fa-caret-right"></i>Dijamin 100% Lolos
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col-lg-6">
+                    <ul className="list">
+                      <li>
+                        <i className="fa fa-caret-right"></i>Universitas Tujuan
+                        Lengkap
+                      </li>
+                      <li>
+                        <i className="fa fa-caret-right"></i>Konselor Lulusan
+                        Korea Selatan
+                      </li>
+                      <li>
+                        <i className="fa fa-caret-right"></i>Kantor di Korea dan
+                        Indonesia
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
